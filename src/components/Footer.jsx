8162bb74a49eda6efd4299bb676b4b7f21e29b9e
Saugat Rimal <saugatrimal60@gmail.com>
2023-02-27 20:58:11 +0545

    footer message

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Footer = () => {
     return (
         <footer className='region footer'>
             <p className='wrapper'>
-                Copyright © Vanlife | Designed by Saugat Rimal
+                Copyright © Vanlife | Designed by Saugat Rimal | View Source <Link to="https://github.com/saugat-rimal/vanlife" target='_blank' >Code on Github</Link>
             </p>
         </footer>
     )
